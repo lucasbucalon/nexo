31e6f232c7e2f629a7b008176ff57d581cd1e9e1
LukasBD <jlucasBucalon@gmail.com>
2026-04-02 00:26:00 -0300

    adiciona estilos e scripts para tooltip-global; remove efeito de contorno em elementos

---
+++ components/models/tooltip/global/script.js
@@ -0,0 +1,9 @@
+(function () {
+  window.Components = window.Components || {};
+  window.Components["tooltip-global"] = {
+    init() {
+      // Componente placeholder: lógica real de tooltip está em src/modules/utils.js
+      // Mantemos init vazio para evitar erros ao chamar window.Components['tooltip-global'].init()
+    },
+  };
+})();
