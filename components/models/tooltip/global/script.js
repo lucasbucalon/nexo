17f30550bd3963fc6ffe0cd42f2c951fb51c6758
LukasBD <jlucasBucalon@gmail.com>
2026-04-03 22:10:55 -0300

    efeito hover duplicado corrigido

--- components/models/tooltip/global/script.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  window.Components = window.Components || {};
-  window.Components["tooltip-global"] = {
-    init() {
-      // Componente placeholder: lógica real de tooltip está em src/modules/utils.js
-      // Mantemos init vazio para evitar erros ao chamar window.Components['tooltip-global'].init()
-    },
-  };
-})();
